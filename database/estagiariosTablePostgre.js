0914788f376fd1424398d644d8a0c3585f5dd0ce
YesusHDS <yesushds123@gmail.com>
2024-03-31 01:09:14 -0300

    Att estagiarios

--- database/estagiariosTablePostgre.js
+++ database/estagiariosTablePostgre.js
@@ -6,6 +6,14 @@ export class estagiariosTablePostgre{
         const {
             cd_curso, 
             cd_registroMatricula, 
+            nm_statusMatricula,
+            nm_turno,
+            nm_ciclo,
+            dt_inicioEstagio,
+            qt_horaEstagioEntrada,
+            qt_horasEstagio,
+            cd_empresa,
+            ic_check,
             dt_periodoComeco, 
             dt_periodoTermino, 
             nm_empresa, 
@@ -15,21 +23,24 @@ export class estagiariosTablePostgre{
         await sql`
             insert into tb_estagiarios 
             (cd_curso, cd_registroMatricula, dt_periodoComeco, 
-                dt_periodoTermino, nm_empresa, nm_estagiario) values
+                dt_periodoTermino, nm_empresa, nm_estagiario, nm_statusMatricula, nm_turno, nm_ciclo, dt_inicioEstagio, qt_horaEstagioEntrada, qt_horasEstagio, cd_empresa, ic_check) values
                 (${cd_curso}, ${cd_registroMatricula}, 
                     ${dt_periodoComeco}, ${dt_periodoTermino}, 
-                    ${nm_empresa}, ${nm_estagiario})
+                    ${nm_empresa}, ${nm_estagiario}, ${nm_statusMatricula}, ${nm_turno}, ${nm_ciclo}, ${dt_inicioEstagio}, ${qt_horaEstagioEntrada}, ${qt_horasEstagio}, ${cd_empresa}, ${ic_check})
         `
     }
 
-    async list(search='', empresa='', RA='', curso=''){
+    async list(search='', empresa='', RA='', curso='', statusMatricula='', check=''){
         let estagiarios = await sql`
-            select e.cd_curso, cd_registroMatricula, dt_periodoComeco, dt_periodoTermino, nm_empresa, nm_estagiario from tb_estagiarios e
+            select e.cd_curso, cd_registroMatricula, dt_periodoComeco, dt_periodoTermino, e.cd_empresa, em.nm_empresa, nm_estagiario, e.nm_turno, e.nm_ciclo, e.dt_inicioEstagio, e.qt_horaEstagioEntrada, e.qt_horasEstagio, e.ic_check, e.nm_statusMatricula from tb_estagiarios e
                 left join tb_cursos c on e.cd_curso = c.cd_curso
+                left join tb_empresas em on e.cd_empresa = em.cd_empresa
                 where nm_estagiario ilike ${'%'+search+'%'} 
-                and nm_empresa ilike ${'%'+empresa+'%'}
+                and em.nm_empresa ilike ${'%'+empresa+'%'}
                 and cd_registroMatricula ilike ${'%'+RA+'%'}
                 and c.cd_curso ilike ${'%'+curso+'%'}
+                and e.nm_statusMatricula ilike ${'%'+statusMatricula+'%'}
+                and e.ic_check ilike ${'%'+check+'%'}
         `
 
         return estagiarios
@@ -41,14 +52,31 @@ export class estagiariosTablePostgre{
             cd_curso, 
             dt_periodoComeco, 
             dt_periodoTermino, 
-            nm_empresa, 
-            nm_estagiario
+            cd_empresa, 
+            nm_estagiario,
+            nm_statusMatricula,
+            nm_turno,
+            nm_ciclo,
+            dt_inicioEstagio,
+            qt_horaEstagioEntrada,
+            qt_horasEstagio,
+            ic_check
         } = estagiario
 
         await sql`
             update tb_estagiarios 
-                set cd_curso = ${cd_curso}, dt_periodoComeco = ${dt_periodoComeco}, dt_periodoTermino = ${dt_periodoTermino},
-                    nm_empresa = ${nm_empresa}, nm_estagiario = ${nm_estagiario}
+                set cd_curso = ${cd_curso}, 
+                    dt_periodoComeco = ${dt_periodoComeco}, 
+                    dt_periodoTermino = ${dt_periodoTermino},
+                    nm_estagiario = ${nm_estagiario},
+                    cd_empresa = ${cd_empresa},
+                    nm_statusMatricula = ${nm_statusMatricula},
+                    nm_turno = ${nm_turno},
+                    nm_ciclo = ${nm_ciclo},
+                    dt_inicioEstagio = ${dt_inicioEstagio},
+                    qt_horaEstagioEntrada = ${qt_horaEstagioEntrada},
+                    qt_horasEstagio = ${qt_horasEstagio},
+                    ic_check = ${ic_check}
                     where cd_registroMatricula = ${id}
         `
     }
