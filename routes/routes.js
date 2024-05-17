b867435d0ae90b4fe5148043f252874fdb2eb016
YesusHDS <yesushds123@gmail.com>
2024-05-17 09:27:42 -0300

    New routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,16 @@
 import {loginRoute} from './loginRoute.js'
 import {cursosRoute} from './cursosRoute.js'
 import {estagiariosRoute} from './estagiariosRoute.js'
+import {ciclosRoute} from './ciclosRoute.js'
+import {empresasRoute} from './empresasRoute.js'
+import {turnosRoute} from './turnosRoute.js'
 
 export const routes = async (server)=>{
     server.register(loginRoute)
     server.register(cursosRoute)
     server.register(estagiariosRoute)
-    
+    server.register(ciclosRoute)
+    server.register(turnosRoute)
+    server.register(empresasRoute)
+
 }
